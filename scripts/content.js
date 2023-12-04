723cca6dc12e4657eb274facff1154d109270d7e
Leon <nonick@nonick.ch>
2023-12-04 20:26:31 +0100

    Add a border to each `<turbo-frame>`

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,9 @@
-console.log("We are up and running!");
+document.querySelectorAll("turbo-frame").forEach((frame) => {
+  frame.style.border = "1px #5CD8E5 solid";
+  frame.style.borderRadius = "5px";
+  frame.style.display = "block";
+
+  const infoBadge = document.createElement("span");
+  infoBadge.textContent = `#${frame.id}`
+  frame.insertAdjacentElement("afterbegin", infoBadge);
+});
