b06c6a9311b6cddcf71f77ae10012f4f99e45a00
Leon <nonick@nonick.ch>
2024-05-21 23:22:03 +0200

    Toggle color picker - only show color picker when turbo frame option is selected

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,15 +2,25 @@ const optionsForm = document.getElementById("optionsForm");
 const watchFramesInput = document.getElementById("watch-frames");
 const frameColorInput = document.getElementById("frames-color");
 
+const toggleFrameColorInput = (show) => {
+  if (show) {
+    frameColorInput.classList.remove("d-none");
+  } else {
+    frameColorInput.classList.add("d-none");
+  }
+}
+
 // Initialize the form with the user's options
 const data = await chrome.storage.sync.get("options");
 const options = data.options || { frames: false, frameColor: "#5cd8e5" };
 
-watchFramesInput.checked = Boolean(options.frames);
+watchFramesInput.checked = options.frames;
 frameColorInput.value = options.frameColor;
+toggleFrameColorInput(options.frames);
 
 // Event listeners to persist selected options
 watchFramesInput.addEventListener("change", (event) => {
+  toggleFrameColorInput(event.target.checked);
   options.frames = event.target.checked;
   chrome.storage.sync.set({ options });
 });
