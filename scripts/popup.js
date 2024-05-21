05cea810abec17e9dd6b941a98c84f5192572594
Leon <nonick@nonick.ch>
2024-05-21 23:15:37 +0200

    Set default options

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,15 +1,15 @@
 const optionsForm = document.getElementById("optionsForm");
 const watchFramesInput = document.getElementById("watch-frames");
 const frameColorInput = document.getElementById("frames-color");
-const options = {};
 
-// Initialize the form with the user's option settings
-const data = await chrome.storage.sync.get("options") || {};
-Object.assign(options, data.options);
+// Initialize the form with the user's options
+const data = await chrome.storage.sync.get("options");
+const options = data.options || { frames: false, frameColor: "#5cd8e5" };
+
 watchFramesInput.checked = Boolean(options.frames);
 frameColorInput.value = options.frameColor;
 
-// Event listeners to persist selected settings
+// Event listeners to persist selected options
 watchFramesInput.addEventListener("change", (event) => {
   options.frames = event.target.checked;
   chrome.storage.sync.set({ options });
