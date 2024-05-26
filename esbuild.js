38abe76992b56f9e43144e2359bc628ccd760de9
Leon <nonick@nonick.ch>
2024-05-26 16:18:03 +0200

    Add `--watch` option to esbuild setup

--- esbuild.js
+++ esbuild.js
@@ -1,19 +1,28 @@
 const esbuild = require("esbuild");
 
-esbuild
-  .build({
-    entryPoints: [
-      "./src/content.js",
-      "./src/popup.js",
-      "./src/inject.js"
-    ],
-    bundle: true,
-    minify: true,
-    sourcemap: process.env.NODE_ENV !== "production",
-    target: ["chrome58", "firefox57"],
-    outdir: "./public/dist",
-    define: {
-      "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`
-    }
-  })
-  .catch(() => process.exit(1));
+const config = {
+  entryPoints: [
+    "./src/content.js",
+    "./src/popup.js",
+    "./src/inject.js"
+  ],
+  bundle: true,
+  minify: true,
+  sourcemap: process.env.NODE_ENV !== "production",
+  target: ["chrome58", "firefox57"],
+  outdir: "./public/dist",
+  define: {
+    "process.env.NODE_ENV": `"${process.env.NODE_ENV}"`
+  }
+}
+
+const buildAndWatch = async () => {
+  const context = await esbuild.context({...config, logLevel: 'info'})
+  context.watch()
+};
+
+if (process.argv.includes("--watch")) {
+  buildAndWatch()
+} else {
+  esbuild.build(config)
+}
