f2de152c117a22a243ce92610a95626c41dda0f5
Leon <nonick@nonick.ch>
2024-05-26 12:15:28 +0200

    Remove Dev Tool panel

--- scripts/content.js
+++ scripts/content.js
@@ -1,89 +1,5 @@
-const connectionPort = chrome.runtime.connect({ name: "knockknock" });
-let connected = false;
-
-connectionPort.onMessage.addListener(function (message) {
-  console.log("content.js: Received message:", message);
-  switch (message.type) {
-    case "CONNECTED":
-      console.debug("content.js: Connected ...");
-      connected = true;
-      break;
-    case "FRAME_LIST":
-      sendFrames();
-      break;
-    case "FRAME_DETAILS":
-      sendFrameDetails(message.frameId);
-      break;
-    case "STIMULUS_LIST":
-      sendStimulusList();
-      break;
-  }
-});
-
 const state = {
   stimulusControllers: []
-}
-
-const sendFrames = async () => {
-  if (!connected) {
-    console.debug("content.js#sendFrames: Not connected yet");
-    return;
-  }
-
-  const frames = Array.from(document.querySelectorAll("turbo-frame")).map((frame) => {
-    return {
-      id: frame.id,
-      src: frame.src
-    };
-  });
-
-  // This will be the first request. Because we can't be sure that the port is
-  // already connected, we'll use chrome.runtime.sendMessage instead of port.postMessage.
-  connectionPort.postMessage({
-    type: "FRAME_LIST",
-    frames: frames
-  });
-}
-
-const sendFrameDetails = async (frameId) => {
-  if (!connected) {
-    console.debug("content.js#sendFrameDetails: Not connected yet");
-    return;
-  }
-
-  const frame = document.getElementById(frameId);
-  const frameDetails = {
-    id: frame.id,
-    src: frame.src
-  };
-
-  connectionPort.postMessage({
-    type: "FRAME_DETAILS",
-    frameDetails: frameDetails
-  });
-}
-
-const sendStimulusList = async () => {
-  if (!connected) {
-    console.debug("content.js#sendStimulusList: Not connected yet");
-    return;
-  }
-
-  const stimulusControllers = Array.from(document.querySelectorAll("[data-controller]")).map((element) => {
-    return {
-      id: element.dataset.controller
-    };
-  });
-
-  connectionPort.postMessage({
-    type: "STIMULUS_LIST",
-    stimulusControllers: stimulusControllers
-  });
-}
-
-const sendCurrentStateToPanel = async () => {
-  sendFrames();
-  sendStimulusList();
 }
 
 const highlightTurboFrames = () => {
@@ -429,10 +345,7 @@ const init = async () => {
 }
 
 const events = ["DOMContentLoaded", "turbolinks:load", "turbo:load", "turbo:frame-load", "hotwire-dev-tools:options-changed"];
-events.forEach((event) => {
-  document.addEventListener(event, sendCurrentStateToPanel);
-  document.addEventListener(event, init);
-});
+events.forEach(event => document.addEventListener(event, init));
 document.addEventListener("turbo:before-stream-render", addTurboStreamToDetailBox);
 
 // Listen for potential message from the injected script
