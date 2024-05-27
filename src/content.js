cde8e817eda32725f11e8296e21b544a0227aa9f
Leon <nonick@nonick.ch>
2024-05-27 23:28:45 +0200

    Save current tab selection

--- src/content.js
+++ src/content.js
@@ -2,17 +2,107 @@ import { html, render } from "lit-html";
 import { getMetaContent } from "./lib/utils";
 import Devtool from "./lib/devtool";
 
-import {
-  createDetailBoxContainer,
-  createDetailBoxHeader,
-  createDetailBoxTabs,
-} from "./lib/element_creation";
-
 const devTool = new Devtool();
 
 const state = {
   stimulusControllers: [],
   turboDetails: {},
+}
+
+const createDetailBoxContainer = () => {
+  const existingContainer = document.getElementById("hotwire-dev-tools-detail-box-container");
+  if (existingContainer) {
+    return existingContainer;
+  }
+  const container = document.createElement("div");
+  container.id = "hotwire-dev-tools-detail-box-container";
+  container.dataset.turboPermanent = true;
+  return container;
+}
+
+const createStimulusTab = () => {
+  const existingTab = document.querySelector("[data-tab-id='hotwire-dev-tools-stimulus-tab']");
+
+  if (existingTab) {
+    return existingTab;
+  }
+  const stimulusTab = document.createElement("button");
+  stimulusTab.classList.add("hotwire-dev-tools-tablink");
+  devTool.options.currentTab == "hotwire-dev-tools-stimulus-tab" && stimulusTab.classList.add("active");
+  stimulusTab.dataset.tabId = "hotwire-dev-tools-stimulus-tab";
+  stimulusTab.innerText = "Stimulus";
+  return stimulusTab;
+}
+
+const createTurboFrameTab = () => {
+  const existingTab = document.querySelector("[data-tab-id='hotwire-dev-tools-turbo-frame-tab']");
+
+  if (existingTab) {
+    return existingTab;
+  }
+  const turboFrameTab = document.createElement("button");
+  turboFrameTab.classList.add("hotwire-dev-tools-tablink");
+  turboFrameTab.dataset.tabId = "hotwire-dev-tools-turbo-frame-tab";
+  devTool.options.currentTab == "hotwire-dev-tools-turbo-frame-tab" && turboFrameTab.classList.add("active");
+  turboFrameTab.innerText = "Frames";
+  return turboFrameTab;
+}
+
+const createTurboStreamTab = () => {
+  const existingTab = document.querySelector("[data-tab-id='hotwire-dev-tools-turbo-stream-tab']");
+
+  if (existingTab) {
+    return existingTab;
+  }
+  const turboStreamTab = document.createElement("button");
+  turboStreamTab.classList.add("hotwire-dev-tools-tablink");
+  devTool.options.currentTab == "hotwire-dev-tools-turbo-stream-tab" && turboStreamTab.classList.add("active");
+  turboStreamTab.dataset.tabId = "hotwire-dev-tools-turbo-stream-tab";
+  turboStreamTab.innerText = "Streams";
+  return turboStreamTab;
+}
+
+const createInfoTab = () => {
+  const existingTab = document.querySelector("[data-tab-id='hotwire-dev-tools-info-tab']");
+
+  if (existingTab) {
+    return existingTab;
+  }
+  const infoTab = document.createElement("button");
+  infoTab.classList.add("hotwire-dev-tools-tablink", "tablink-with-icon");
+  devTool.options.currentTab == "hotwire-dev-tools-info-tab" && infoTab.classList.add("active");
+  infoTab.dataset.tabId = "hotwire-dev-tools-info-tab";
+  infoTab.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>`;
+  return infoTab;
+}
+
+const createDetailBoxHeader = () => {
+  const existingHeader = document.querySelector(".hotwire-dev-tools-detail-box-header");
+  if (existingHeader) {
+    return existingHeader;
+  }
+  const header = document.createElement("div");
+  header.classList.add("hotwire-dev-tools-detail-box-header");
+  return header;
+}
+
+const createDetailBoxTabList = () => {
+  const existingTablist = document.querySelector(".hotwire-dev-tools-tablist");
+  if (existingTablist) {
+    return existingTablist;
+  }
+  const tablist = document.createElement("div");
+  tablist.classList.add("hotwire-dev-tools-tablist");
+  return tablist;
+}
+
+const createDetailBoxTabs = () => {
+  const tablist = createDetailBoxTabList();
+  tablist.appendChild(createStimulusTab());
+  tablist.appendChild(createTurboFrameTab());
+  tablist.appendChild(createTurboStreamTab());
+  tablist.appendChild(createInfoTab());
+  return tablist;
 }
 
 const highlightTurboFrames = () => {
@@ -107,6 +197,7 @@ const createTurboStreamDetailBoxContent = () => {
   const content = document.createElement("div");
   content.classList.add("hotwire-dev-tools-tab-content");
   content.id = "hotwire-dev-tools-turbo-stream-tab";
+  devTool.options.currentTab == "hotwire-dev-tools-turbo-stream-tab" && content.classList.add("active");
 
   return content
 }
@@ -120,6 +211,7 @@ const createTurboFrameDetailBoxContent = () => {
   const content = document.createElement("div");
   content.classList.add("hotwire-dev-tools-tab-content");
   content.id = "hotwire-dev-tools-turbo-frame-tab";
+  devTool.options.currentTab == "hotwire-dev-tools-turbo-frame-tab" && content.classList.add("active");
 
   document.querySelectorAll("turbo-frame").forEach((frame) => {
     const entry = document.createElement("div");
@@ -138,8 +230,9 @@ const createStimulusDetailBoxContent = () => {
   }
 
   const content = document.createElement("div");
-  content.classList.add("hotwire-dev-tools-tab-content", "active");
+  content.classList.add("hotwire-dev-tools-tab-content");
   content.id = "hotwire-dev-tools-stimulus-tab";
+  devTool.options.currentTab == "hotwire-dev-tools-stimulus-tab" && content.classList.add("active");
 
   const groupedStimulusControllers = groupedStimulusControllerElements();
   for (const stimulusControllerId in groupedStimulusControllers) {
@@ -181,6 +274,7 @@ const createInfoBoxContent = () => {
   const content = document.createElement("div");
   content.classList.add("hotwire-dev-tools-tab-content");
   content.id = "hotwire-dev-tools-info-tab";
+  devTool.options.currentTab == "hotwire-dev-tools-info-tab" && content.classList.add("active");
 
   const turboContent = html`
     <div class="info-tab-content">
@@ -275,6 +369,8 @@ const listenForTabNavigation = () => {
 
     clickedTab.classList.add("active");
     desiredTabContent.classList.add("active");
+
+    devTool.saveOptions({ currentTab: clickedTab.dataset.tabId });
   })
 }
 
