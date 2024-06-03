8aa34b905a1efd28df1375c490f1d5f5db94babf
Leon <lv@oxon.ch>
2024-06-03 11:34:54 +0200

    Refactor `inject.js`

--- src/inject.js
+++ src/inject.js
@@ -1,3 +1,7 @@
+// This script can be injected into the active page by the content script
+// The purpose is to access the page's `window` object, which is inaccessible from the content script
+// We gather details about the current page and sends them back to the content script via `window.postMessage`
+
 const sendRegisteredControllers = () => {
   const registeredControllers = window.Stimulus?.router.modulesByIdentifier.keys()
   window.postMessage(
@@ -11,10 +15,14 @@ const sendRegisteredControllers = () => {
 }
 
 const sendTurboDetails = () => {
-  const details = {
-    turboDriveEnabled: window.Turbo?.session.drive,
-  }
-  window.postMessage({ source: "inject", message: "turboDetails", details: details }, window.location.origin)
+  window.postMessage(
+    {
+      source: "inject",
+      message: "turboDetails",
+      details: { turboDriveEnabled: window.Turbo?.session.drive },
+    },
+    window.location.origin,
+  )
 }
 
 const sendWindowDetails = () => {
@@ -22,9 +30,6 @@ const sendWindowDetails = () => {
   sendTurboDetails()
 }
 
-const events = ["DOMContentLoaded", "turbolinks:load", "turbo:load", "turbo:frame-load"]
-events.forEach((event) => {
-  document.addEventListener(event, sendWindowDetails)
-})
-
+const events = ["DOMContentLoaded", "turbolinks:load", "turbo:load"]
+events.forEach((event) => document.addEventListener(event, sendWindowDetails, { passive: true }))
 sendWindowDetails()
