cdb2816d9b2dbb81739983bfe8098132a5ec1224
Leon <lv@oxon.ch>
2024-06-03 15:21:19 +0200

    Add basic Safari extension

---
+++ HotwireDevTools/HotwireDevTools/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  HotwireDevTools
+//
+//  Created by Leon on 03.06.2024.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
