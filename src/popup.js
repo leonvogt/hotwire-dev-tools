4c9589661a1cec3bb8b9ad75c4cd1013629e2018
Enrico <54737363+lirtistan@users.noreply.github.com>
2024-06-13 09:16:15 +0200

    also added a firefox condition

--- src/popup.js
+++ src/popup.js
@@ -247,7 +247,8 @@ const getCurrentTabOrigin = async () => {
     document.querySelector(".page-specific-options-wrapper").remove()
   }
 
-  versionString.textContent = chrome.runtime.getManifest().version
+  versionString.textContent = devTool.isFirefox ? browser.runtime.getManifest().version : chrome.runtime.getManifest().version
+
   const options = await devTool.getOptions()
   initializeForm(options)
   setupEventListeners(options)
