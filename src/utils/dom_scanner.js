43a81be5af1ad003cd6b726224c418716f246466
Leon <nonick@nonick.ch>
2024-10-19 13:53:43 +0200

    Introduce `DOMScanner`

---
+++ src/utils/dom_scanner.js
@@ -0,0 +1,50 @@
+export default class DOMScanner {
+  static SHADOW_CONTAINER_ID = "hotwire-dev-tools-shadow-container"
+  static TURBO_FRAME_OVERLAY_CLASS_NAME = "hotwire-dev-tools-highlight-overlay-turbo-frame"
+
+  // Turbo
+  static get turboFrameElements() {
+    return document.querySelectorAll("turbo-frame")
+  }
+
+  // Stimulus
+  static get stimulusControllerElements() {
+    return document.querySelectorAll("[data-controller]")
+  }
+
+  static get stimulusControllerIdentifiers() {
+    return Array.from(this.stimulusControllerElements)
+      .map((element) => element.dataset.controller.split(" "))
+      .flat()
+  }
+
+  static get uniqueStimulusControllerIdentifiers() {
+    return [...new Set(this.stimulusControllerIdentifiers)]
+  }
+
+  static get groupedStimulusControllerElements() {
+    const groupedElements = {}
+    this.stimulusControllerElements.forEach((element) => {
+      element.dataset.controller
+        .split(" ")
+        .filter((stimulusControllerId) => stimulusControllerId.trim() !== "")
+        .forEach((stimulusControllerId) => {
+          if (!groupedElements[stimulusControllerId]) {
+            groupedElements[stimulusControllerId] = []
+          }
+          groupedElements[stimulusControllerId].push(element)
+        })
+    })
+
+    return groupedElements
+  }
+
+  // Dev Tools
+  static get shadowContainer() {
+    return document.getElementById(this.SHADOW_CONTAINER_ID)
+  }
+
+  static get turboFrameOverlayElements() {
+    return document.querySelectorAll(`.${this.TURBO_FRAME_OVERLAY_CLASS_NAME}`)
+  }
+}
