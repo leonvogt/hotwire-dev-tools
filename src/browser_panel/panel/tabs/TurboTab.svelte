e4f536b201c4d73632997f45617ec831bea57296
Leon <nonick@nonick.ch>
2025-06-10 10:57:20 +0200

    Fix layout issues

--- src/browser_panel/panel/tabs/TurboTab.svelte
+++ src/browser_panel/panel/tabs/TurboTab.svelte
@@ -6,7 +6,7 @@
   hljs.registerLanguage("xml", xml)
 
   import { getTurboFrames, getTurboStreams } from "../../State.svelte.js"
-  import { inspectElement } from "../../../utils/utils.js"
+  import { inspectElement, debounce } from "../../../utils/utils.js"
   import { panelPostMessage } from "../../messaging.js"
   import { PANEL_TO_BACKEND_MESSAGES } from "../../../lib/constants.js"
   import { HOTWIRE_DEV_TOOLS_PANEL_SOURCE } from "../../ports.js"
@@ -17,6 +17,8 @@
     TURBO_FRAME: "turbo-frame",
     TURBO_STREAM: "turbo-stream",
   }
+  const turboStreamAnimationDuration = 300
+  const ignoredAttributes = ["id", "loading", "src", "complete", "aria-busy", "busy"]
 
   let turboFrames = $state([])
   let turboStreams = $state([])
@@ -61,11 +63,9 @@
     }
   })
 
-  const scrollIntoView = (element) => {
-    // setTimeout(() => {
-    //   element.scrollIntoView({ behavior: "smooth", block: "end" })
-    // }, turboStreamAnimationDuration)
-  }
+  const scrollIntoView = debounce((element) => {
+    element.scrollIntoView({ behavior: "smooth", block: "end" })
+  }, turboStreamAnimationDuration)
 
   const addHighlightOverlay = (selector) => {
     panelPostMessage({
@@ -136,84 +136,82 @@
       }
     }, 10)
   }
-
-  const turboStreamAnimationDuration = 300
-  const ignoredAttributes = ["id", "loading", "src", "complete", "aria-busy", "busy"]
 </script>
 
 <Splitpanes class="turbo-frames-list-panel" horizontal={$horizontalPanes}>
   <Pane size={35}>
-    <div class="d-flex justify-content-center">
-      <h2>Streams</h2>
+    <div class="d-flex flex-column h-100">
+      <div class="d-flex justify-content-center">
+        <h2>Streams</h2>
+      </div>
+      {#if turboStreams.length > 0}
+        <div class="scrollable-list">
+          {#each turboStreams as stream}
+            <div
+              {@attach scrollIntoView}
+              class="entry-row p-1 cursor-pointer"
+              transition:slide={{ duration: turboStreamAnimationDuration }}
+              class:selected={selected.type === SELECTABLE_TYPES.TURBO_STREAM && selected.uuid === stream.uuid}
+              role="button"
+              tabindex="0"
+              onclick={() => setSelectedTurboStream(stream)}
+              onkeyup={() => setSelectedTurboStream(stream)}
+            >
+              <div class="text-align-right text-muted">
+                <span class="timestamp">{stream.time}</span>
+              </div>
+              <div class="d-flex justify-content-between align-items-center">
+                <div>{stream.action}</div>
+                <div>{stream.targetSelector}</div>
+              </div>
+            </div>
+          {/each}
+        </div>
+      {:else}
+        <div class="no-entry-hint">
+          <span>No Turbo Streams seen yet</span>
+          <span>We'll keep looking</span>
+        </div>
+      {/if}
     </div>
-    {#if turboStreams.length > 0}
-      <button class="btn-icon icon-dark position-absolute end-0" onclick={refreshTurboFrames} title="Refresh Turbo Frames List">
-        {@html Icons.refresh}
-      </button>
-      <div class="scrollable-list">
-        {#each turboStreams as stream}
-          <div
-            {@attach scrollIntoView}
-            class="entry-row p-1 cursor-pointer"
-            transition:slide={{ duration: turboStreamAnimationDuration }}
-            class:selected={selected.type === SELECTABLE_TYPES.TURBO_STREAM && selected.uuid === stream.uuid}
-            role="button"
-            tabindex="0"
-            onclick={() => setSelectedTurboStream(stream)}
-            onkeyup={() => setSelectedTurboStream(stream)}
-          >
-            <div class="text-align-right text-muted">
-              <span class="timestamp">{stream.time}</span>
-            </div>
-            <div class="d-flex justify-content-between align-items-center">
-              <div>{stream.action}</div>
-              <div>{stream.targetSelector}</div>
-            </div>
-          </div>
-        {/each}
-      </div>
-    {:else}
-      <div class="no-entry-hint">
-        <span>No Turbo Streams seen yet</span>
-        <span>We'll keep looking</span>
-      </div>
-    {/if}
   </Pane>
-  <Pane size={35}>
-    <div class="d-flex justify-content-center align-items-center position-relative">
-      <h2>Frames</h2>
-      <button class="btn-icon icon-dark position-absolute end-0" onclick={refreshTurboFrames} title="Refresh Turbo Frames List">
-        {@html Icons.refresh}
-      </button>
-    </div>
 
-    {#if turboFrames.length > 0}
-      <div class="scrollable-list">
-        {#each turboFrames as frame}
-          {@const selector = `#${frame.id}`}
-          <div
-            class="p-1 d-flex justify-content-between align-items-center cursor-pointer entry-row"
-            class:selected={selected.type === SELECTABLE_TYPES.TURBO_FRAME && selected.frame.id === frame.id}
-            role="button"
-            tabindex="0"
-            onclick={() => setSelectedTurboFrame(frame)}
-            onkeyup={() => setSelectedTurboFrame(frame)}
-            onmouseenter={() => addHighlightOverlay(selector)}
-            onmouseleave={() => hideHighlightOverlay()}
-          >
-            <div>{selector}</div>
-            <button class="btn-icon btn-inspect" onclick={() => inspectElement(selector)}>
-              {@html Icons.inspectElement}
-            </button>
-          </div>
-        {/each}
+  <Pane size={35}>
+    <div class="d-flex flex-column h-100">
+      <div class="d-flex justify-content-center align-items-center position-relative">
+        <h2>Frames</h2>
+        <button class="btn-icon icon-dark position-absolute end-0" onclick={refreshTurboFrames} title="Refresh Turbo Frames List">
+          {@html Icons.refresh}
+        </button>
       </div>
-    {:else}
-      <div class="no-entry-hint">
-        <span>No Turbo Frames found on this page</span>
-        <span>We'll keep looking</span>
-      </div>
-    {/if}
+      {#if turboFrames.length > 0}
+        <div class="scrollable-list">
+          {#each turboFrames as frame}
+            {@const selector = `#${frame.id}`}
+            <div
+              class="p-1 d-flex justify-content-between align-items-center cursor-pointer entry-row"
+              class:selected={selected.type === SELECTABLE_TYPES.TURBO_FRAME && selected.frame.id === frame.id}
+              role="button"
+              tabindex="0"
+              onclick={() => setSelectedTurboFrame(frame)}
+              onkeyup={() => setSelectedTurboFrame(frame)}
+              onmouseenter={() => addHighlightOverlay(selector)}
+              onmouseleave={() => hideHighlightOverlay()}
+            >
+              <div>{selector}</div>
+              <button class="btn-icon btn-inspect" onclick={() => inspectElement(selector)}>
+                {@html Icons.inspectElement}
+              </button>
+            </div>
+          {/each}
+        </div>
+      {:else}
+        <div class="no-entry-hint">
+          <span>No Turbo Frames found on this page</span>
+          <span>We'll keep looking</span>
+        </div>
+      {/if}
+    </div>
   </Pane>
 
   <Pane size={30} class="turbo-frames-detail-panel flow">
@@ -251,7 +249,9 @@
 
       <div class="pane-section-heading">HTML</div>
       <div class="d-flex justify-content-between align-items-top gap-2">
-        <pre class="html-preview"><code class="language-html">{@html hljs.highlight(selected.frame.html, { language: "html" }).value}</code></pre>
+        <div class="html-preview">
+          <pre><code class="language-html">{@html hljs.highlight(selected.frame.html, { language: "html" }).value}</code></pre>
+        </div>
         <button class="btn-icon btn-inspect" onclick={() => inspectElement(`#${selected.frame.id}`)}>
           {@html Icons.inspectElement}
         </button>
@@ -260,31 +260,32 @@
       <div class="d-flex justify-content-center align-items-center position-relative">
         <h2>{selected.stream.action} {selected.stream.targetSelector}</h2>
       </div>
+      <div class="scrollable-list flow">
+        <div class="pane-section-heading">Attributes</div>
+        <table class="table table-sm w-100">
+          <tbody>
+            <tr>
+              <td>Action</td>
+              <td>{selected.stream.action}</td>
+            </tr>
+            <tr>
+              <td>Target</td>
+              <td>
+                <div class="d-flex justify-content-between align-items-center">
+                  <span>{selected.stream.targetSelector}</span>
+                  <button class="btn-icon btn-inspect" onclick={() => inspectElement(`${selected.stream.targetSelector}`)}>
+                    {@html Icons.inspectElement}
+                  </button>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
 
-      <div class="pane-section-heading">Attributes</div>
-      <table class="table table-sm w-100">
-        <tbody>
-          <tr>
-            <td>Action</td>
-            <td>{selected.stream.action}</td>
-          </tr>
-          <tr>
-            <td>Target Selector</td>
-            <td>
-              <div class="d-flex justify-content-between align-items-center">
-                <span>{selected.stream.targetSelector}</span>
-                <button class="btn-icon btn-inspect" onclick={() => inspectElement(`${selected.stream.targetSelector}`)}>
-                  {@html Icons.inspectElement}
-                </button>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-
-      <div class="pane-section-heading">HTML</div>
-      <div class="d-flex justify-content-between align-items-top gap-2">
-        <pre class="html-preview"><code class="language-html">{@html hljs.highlight(selected.stream.turboStreamContent, { language: "html" }).value}</code></pre>
+        <div class="pane-section-heading">HTML</div>
+        <div class="html-preview">
+          <pre><code class="language-html">{@html hljs.highlight(selected.stream.turboStreamContent, { language: "html" }).value}</code></pre>
+        </div>
       </div>
     {:else}
       <div class="no-entry-hint">
