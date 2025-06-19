0c79bfd1c1041c10ab9d9954e81853a3b340de50
Leon <lv@oxon.ch>
2025-06-20 01:42:45 +0200

    Add support for highlighting elements inside a `<dialog>`

--- src/utils/highlight.js
+++ src/utils/highlight.js
@@ -11,22 +11,27 @@ export const addHighlightOverlayToElements = (elementsOrSelector, color = "#007a
 
   elements.forEach((element) => {
     const rect = element.getBoundingClientRect()
-    createOverlay(rect, color, overlayClassName, opacity)
+
+    // If the element is inside a dialog, use the dialog as the container
+    // If we don't place the highlight inside the dialog, it will be clipped by the dialog
+    const container = element.closest("dialog") || document.body
+    createOverlay(rect, color, overlayClassName, opacity, container)
   })
 }
 
-export const createOverlay = (rect, color, overlayClassName, opacity) => {
+export const createOverlay = (rect, color, overlayClassName, opacity, container = document.body) => {
   const overlay = document.createElement("div")
   overlay.className = overlayClassName
   overlay.style.position = "absolute"
-  overlay.style.zIndex = 9999999
+  overlay.style.zIndex = 2147483647 // Highest possible z-index
   overlay.style.opacity = opacity
   overlay.style.top = `${rect.top + window.scrollY}px`
   overlay.style.left = `${rect.left + window.scrollX}px`
   overlay.style.width = `${rect.width}px`
   overlay.style.height = `${rect.height}px`
   overlay.style.backgroundColor = color
-  document.body.appendChild(overlay)
+  overlay.style.pointerEvents = "none"
+  container.appendChild(overlay)
 }
 
 export const removeHighlightOverlay = (selector = ".hotwire-dev-tools-highlight-overlay") => {
